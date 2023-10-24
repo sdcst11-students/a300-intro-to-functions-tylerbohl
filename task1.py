96eae705318d194b61e627a1cbaabc2fc7edf6d7
tylerbohl <144383251+tylerbohl@users.noreply.github.com>
2023-10-24 12:58:06 -0700

    oct 24

--- task1.py
+++ task1.py
@@ -1,6 +1,21 @@
+
+
 def A():
+    print("Hello")
 
 def B():
-
+    print("How are you?")
 
 def C():
+    print("Hi")
+
+x = input("Enter A, B, or C ")
+
+if x == "A" or x == "a":
+    A()
+
+if x == "B" or x == "b":
+    B()
+
+if x == "C" or x == "c":
+    C()
--- task2.py
+++ task2.py
@@ -8,3 +8,25 @@ displays instructions and how to play
 game()
 plays the games
 """
+y = 96
+def title():
+    print("NUMBER GUESSING GAME\nYou have 10 guesses\neach wrong geuss will tell you if you too high or too low\nGood Luck")
+
+def game():
+    for i in range(10):
+        x = input("Enter an integer: ")
+        x = int(x)
+        if x == y:
+            print(f"You won in {i+1} geusses")
+            break
+        elif x > y: 
+           print("Too high")
+        elif x < y: 
+           print("Too low")
+        elif i == 9:
+            print("Game Over")
+            break
+
+title()
+
+game()
